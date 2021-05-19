bbfa9075993e62e74434cb52b74c64e60b87cc74
Lucas <lucas.67817@gmail.com>
2021-05-18 21:52:59 -0300

    Novas funçẽos

--- Controllers/CadastroController.cs
+++ Controllers/CadastroController.cs
@@ -82,6 +82,8 @@ namespace ControleEstoque.Controllers
             }
                 return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
         }
+
+       
         // GET: Cadastro
         private static List<MarcaProduto> _listaMarcaProduto = new List<MarcaProduto>()
         {
@@ -164,10 +166,70 @@ namespace ControleEstoque.Controllers
 
         private static List<Produto> _listaProduto = new List<Produto>()
         {
-            new Produto() { Id=1, Nome="Notebook"},
-            new Produto() { Id=2, Nome="Mouse"},
-            new Produto() { Id=3, Nome="Teclado"},
+            new Produto() { Id=1, Nome="Notebook Dell"},
+            new Produto() { Id=2, Nome="Mouse Gamer Logitech G402"},
+            new Produto() { Id=3, Nome="Mouse Dell"},
+            new Produto() { Id=4, Nome="Teclado Dell"},
+            new Produto() { Id=5, Nome="Monitor Dell"},
+            new Produto() { Id=6, Nome="Computador Dell"},
+            new Produto() { Id=7, Nome="Computador Positivo"},
+            new Produto() { Id=8, Nome="Notebook Acer Aspire 4740"},
+            new Produto() { Id=9, Nome="Computador Acer"},
         };
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SalvarProduto(Produto model)
+        {
+            var resultado = "OK";
+            var mensagens = new List<string>();
+            var idSalvo = string.Empty;
+            if (!ModelState.IsValid)
+            {
+                resultado = "AVISO";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    var registroBD = _listaProduto.Find(x => x.Id == model.Id);
+                    if (registroBD == null)
+                    {
+                        registroBD = model;
+                        registroBD.Id = _listaProduto.Max(x => x.Id) + 1;
+                        _listaProduto.Add(registroBD);
+                    }
+                    else
+                    {
+                        registroBD.Nome = model.Nome;
+                        registroBD.Ativo = model.Ativo;
+                    }
+
+                    idSalvo = registroBD.Id.ToString();
+                }
+                catch (Exception ex)
+                {
+                    resultado = "ERRO";
+
+                }
+            }
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult ExcluirProduto(int id)
+        {
+            var ret = false;
+            var registroBD = _listaProduto.Find(x => x.Id == id);
+            if (registroBD != null)
+            {
+                _listaProduto.Remove(registroBD);
+                ret = true;
+            }
+            return Json(ret);
+        }
 
         [Authorize]
         public ActionResult Produto()
